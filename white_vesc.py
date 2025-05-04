54e1dd43032f2793a5c1ecd823d0fc76459753a5
Ilya <test@test.ru>
2025-05-04 18:05:07 +0300

    fix

--- white_vesc.py
+++ white_vesc.py
@@ -426,7 +426,7 @@ while running:
     summ_current = data['slave']['motor_current'] + data['master']['motor_current']
     draw_arc(f"{int(summ_current)}A", screen, (WIDTH * 0.2, 370), 80, summ_current, 200, (255, 0, 0))
     summ_battery = data['slave']['battery_current'] + data['master']['battery_current']
-    draw_arc(f"{int(summ_battery)}A", screen, (WIDTH * 0.5, 370), 80, summ_battery, 50, (0, 0, 255))
+    draw_arc(f"{int(summ_battery / 2)}A", screen, (WIDTH * 0.5, 370), 80, summ_battery, 50, (0, 0, 255))
     average_duty = int((data['slave']['duty'] + data['master']['duty']) / 2)
     draw_arc(f"{int(average_duty)}%", screen, (WIDTH * 0.8, 370), 80, average_duty, 100, (0, 0, 0))
 
