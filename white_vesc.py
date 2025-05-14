ccf4c66f524838e1da5efa508f9616fae4f2bd81
Ilya <test@test.ru>
2025-05-14 22:59:57 +0300

    f

--- white_vesc.py
+++ white_vesc.py
@@ -967,7 +967,8 @@ while running:
     prev_speed = int(data['speed'])
 
     if not zamer_success_prev and zamer_success:
-      add_speak_message("Разгон " + f"{measured_time:.2f}".replace(".", " и ") + "... пиздец медленно")
+      add_speak_message("Разгон " + f"{measured_time:.2f}".replace(".", " и "))
+      add_speak_message("Пиздец медленно")
     zamer_success_prev = zamer_success
 
     razg_boost = 260
