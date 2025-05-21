6374904416e8f7eb026a55ea98091d9067ab5122
Ilya <test@test.ru>
2025-05-21 10:49:02 +0300

    f

--- white_vesc.py
+++ white_vesc.py
@@ -1005,22 +1005,28 @@ while running:
 
     # температура колес дергается поэтому после смены ждем время до следующего уведомления
     cur_t = time.time()
-    if cur_t - PREV_VALS['motor1_temp_get_last_time'] > 10:
+    if cur_t - PREV_VALS['motor1_temp_get_last_time'] > 15:
       if data['slave']['temp_motor'] >= 60 and PREV_VALS['motor1_temp'] < 60:
         add_speak_message(f"Температура переднего колеса достигла... {data['slave']['temp_motor']} градусов")
         PREV_VALS['motor1_temp_get_last_time'] = cur_t
       if data['slave']['temp_motor'] >= 80 and PREV_VALS['motor1_temp'] < 80:
+        add_speak_message(f"Температура переднего колеса достигла... {data['slave']['temp_motor']} градусов")
+        PREV_VALS['motor1_temp_get_last_time'] = cur_t
+      if data['slave']['temp_motor'] >= 90 and PREV_VALS['motor1_temp'] < 90:
         add_speak_message(f"Внимание... Температура переднего колеса достигла... {data['slave']['temp_motor']} градусов")
         PREV_VALS['motor1_temp_get_last_time'] = cur_t
       if data['slave']['temp_motor'] >= 100 and PREV_VALS['motor1_temp'] < 100:
         add_speak_message(f"Опсано перегрев... Температура переднего колеса достигла... {data['slave']['temp_motor']} градусов")
         PREV_VALS['motor1_temp_get_last_time'] = cur_t
 
-    if cur_t - PREV_VALS['motor2_temp_get_last_time'] > 10:
+    if cur_t - PREV_VALS['motor2_temp_get_last_time'] > 15:
       if data['master']['temp_motor'] >= 60 and PREV_VALS['motor2_temp'] < 60:
         add_speak_message(f"Температура заднего колеса достигла... {data['master']['temp_motor']} градусов")
         PREV_VALS['motor2_temp_get_last_time'] = cur_t
       if data['master']['temp_motor'] >= 80 and PREV_VALS['motor2_temp'] < 80:
+        add_speak_message(f"Температура заднего колеса достигла... {data['master']['temp_motor']} градусов")
+        PREV_VALS['motor2_temp_get_last_time'] = cur_t
+      if data['master']['temp_motor'] >= 90 and PREV_VALS['motor2_temp'] < 90:
         add_speak_message(f"Внимание... Температура заднего колеса достигла... {data['master']['temp_motor']} градусов")
         PREV_VALS['motor2_temp_get_last_time'] = cur_t
       if data['master']['temp_motor'] >= 100 and PREV_VALS['motor2_temp'] < 100:
